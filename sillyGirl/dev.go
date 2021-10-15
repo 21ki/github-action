73d6125254114621d1b6df6a9be5149d143e0124
Myki <21kixc@gmail.com>
2021-10-15 15:18:18 +0800

    Update dev.go

--- sillyGirl/dev.go
+++ sillyGirl/dev.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-     //青龙2.9机器人
-	 _ "github.com/cdle/sillyGirl/develop/qinglong"
-	 //京东账号
-	 _ "github.com/cdle/sillyGirl/develop/jd_cookie"
-	 //腾讯QQ
-	 _ "github.com/cdle/sillyGirl/im/qq"
-	 //telegram
-	 _ "github.com/cdle/sillyGirl/im/tg"
+
+//青龙2.9机器人
+_"github.com/cdle/sillyGirl/develop/qinglong" 
+
+//京东账号
+_ "github.com/cdle/sillyGirl/develop/jd_cookie"
+
+//腾讯QQ
+_"github.com/cdle/sillyGirl/im/qq"
+
+//Te Legram
+_"github.com/cdle/sillyGirl/im/tg"
+
+//微信公众号
+_"github.com/cdle/sillyGirl/im/wxmp"
+
+//微信
+_"github.com/cdle/sillyGirl/im/wx"
 )
